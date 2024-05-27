a1d4a7fffcb6b941a4f0629ba26ef5d5e18194ac
Slava-inc <v.danchenko@voshod.local>
2024-05-27 17:53:14 +0300

    post update

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostsList, PostDetail, PostSearch, PostCreate
+from .views import PostsList, PostDetail, PostSearch, PostCreate, PostUpdate
 
 urlpatterns = [
     path('', PostsList.as_view()),
     path('<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('search/', PostSearch.as_view(), name='search'),
     path('create/', PostCreate.as_view(), name='post_create'),
+    path('<int:pk>/update', PostUpdate.as_view(), name='post_update'),
     ]
