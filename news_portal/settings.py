bebd9afb5ee3641020024856ebaef79da6cb97e1
Slava-inc <v.danchenko@voshod.local>
2024-06-12 11:58:16 +0300

    articale  mail sending

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 
     'django.contrib.sites',
     'django.contrib.flatpages',
-    'news',
+    'news.apps.NewsConfig',
     'sign',
     'protect',
     'allauth',
@@ -159,7 +159,7 @@ AUTHENTICATION_BACKENDS = (
 EMAIL_HOST = 'smtp.yandex.ru'  # адрес сервера Яндекс-почты для всех один и тот же
 EMAIL_PORT = 465  # порт smtp сервера тоже одинаковый
 EMAIL_HOST_USER = 'slavikdanchenko'  # ваше имя пользователя, например, если ваша почта user@yandex.ru, то сюда надо писать user, иными словами, это всё то что идёт до собаки
-EMAIL_HOST_PASSWORD = 'here my password was'  # пароль от почты
+EMAIL_HOST_PASSWORD = 'here was password'  # пароль от почты
 EMAIL_USE_SSL = True  # Яндекс использует ssl, подробнее о том, что это, почитайте в дополнительных источниках, но включать его здесь обязательно
 
 SITE_URL = 'http://127.0.0.1:8000'
